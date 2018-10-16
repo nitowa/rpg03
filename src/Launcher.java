f7b0a47f4e1166398f152a43e68150e091cef164
PM <e1350868@student.tuwien.ac.at>
2018-10-16 17:38:30 +0200

    checkpoint

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,3 +1,6 @@
+import java.io.File;
+import java.net.URL;
+
 import GameLogic.State.UI.Img2Ascii;
 import GameLogic.State.UI.JukeBox;
 import myFirstGame.myFirstGame;
@@ -6,6 +9,9 @@ public class Launcher {
 
     public static void main(String[] args) {
 
+        for(String s : System.getProperty("java.class.path").split(";")){
+            System.out.println(s);
+        }
         new myFirstGame();
     }
 }
